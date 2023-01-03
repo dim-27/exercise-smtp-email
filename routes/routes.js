71e9037379fda5385549580c8b1f32180560ce46
dim-27 <dimasfirmanda27@gmail.com>
2023-01-03 17:44:28 +0700

    last task: - add authentication - add transporter

--- routes/routes.js
+++ routes/routes.js
@@ -11,5 +11,18 @@ const isDev = process.env.NODE_ENV != 'production';
 const transport = {
     host: "smtp.gmail.com",
     port: 465,
-    
+    secure: true,
+    auth: {
+        user: process.env.SMTP_TO_EMAIL,
+        password: process.env.SMTP_TO_PASSWORD
+    }
 }
+
+const transporter = nodemailer.createTransport(transport);
+transporter.verify((error, success) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("ready to send");
+    }
+})
